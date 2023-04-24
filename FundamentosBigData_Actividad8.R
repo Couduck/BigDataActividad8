75e8147f2648cc2eca4fd75c163ff2fd5083fb51
Patricio <patoestradac@gmail.com>
2023-04-24 12:01:37 -0500

    Actividad completada

---
+++ FundamentosBigData_Actividad8.R
@@ -0,0 +1,23 @@
+# Primer ejercicio de R de la actividad 8
+answer <- 0
+for (j in 3:5)
+{
+  answer <- j+answer
+}
+print (answer)
+
+# Segundo ejercicio de R de la actividad 8
+answer <- 10
+for (j in 3:5)
+{
+  answer <- j+answer
+}
+print (answer)
+
+# Tercer ejercicio de R de la actividad 8
+answer <- 10
+for (j in 3:5)
+{
+  answer <- j*answer
+}
+print (answer)
